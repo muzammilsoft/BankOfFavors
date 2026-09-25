2e9b072a2b373f98e334b02b8c9492b6d3ee0ef2
Muzammil Software <96596267+muzammilsoft@users.noreply.github.com>
2026-09-25 14:16:59 +0200

    v2.0.0: bump major version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kgsoft.favorsbank"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.9.0"
+        versionCode = 11
+        versionName = "2.0.0"
         vectorDrawables {
             useSupportLibrary = true
         }
